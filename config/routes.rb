839befed93f799d8341aa86a256acdecee876d04
ahzunzun <looyongj2000@hotmail.com>
2024-05-07 18:36:34 +1000

    implemented routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#index"
+  namespace :api do
+    namespace :v1 do
+    resource :airlines, param: :slug
+    resource :reviews, only: [:create, :destroy]
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "*path", to: "pages#index", via: :all
 end
